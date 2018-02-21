8310d1866515a428920dd76e48bb5256dd66cffe
Fernando Sancho <toptnc@gmail.com>
2018-02-21 23:29:51 +0100

    Changed the scrape method. Hope this will last longer.

--- get-arenavision.sh
+++ get-arenavision.sh
@@ -4,14 +4,20 @@ m3ufile=$HOME/Escritorio/arenavision.m3u
 
 echo > $m3ufile
 progress=0
+
+links=$(curl -s --cookie "beget=begetok" http://arenavision.in | grep -o '\<a href.*\>' | sed 's/\<a\ href/\n\<a\ href/g' | grep ArenaVision)
 (
-for i in $(seq -w 1 31);
-do
-   arenalink=$(curl -s --cookie "beget=begetok"  http://arenavision2017.cf/$i | grep acestream:// | sed 's/\ /\n/g'| grep acestream | cut -d "=" -f 2 | sed 's/\"//g')
-   echo \#EXTINF:-1,Arenavision AV$i >> $m3ufile
-   echo $arenalink >> $m3ufile
-   progress=$(($progress + 3))
-   echo $progress
+    IFS='
+'
+    for line in $links;
+    do
+	arenaurl=$(echo "$line" | cut -d '"' -f 2)
+	arenatitle=$(echo "$line" | cut -d '>' -f 2 | cut -d '<' -f 1)
+	arenalink=$(curl -s --cookie "beget=begetok" $arenaurl | grep acestream:// | sed 's/\ /\n/g'| grep acestream | cut -d "=" -f 2 | sed 's/\"//g')
+	echo \#EXTINF:-1,"$arenatitle" >> $m3ufile
+	echo $arenalink >> $m3ufile
+	progress=$(($progress + 3))
+	echo $progress
 done
 echo 100
 ) | yad --center \
@@ -24,8 +30,8 @@ echo 100
 
 guidetemp='/tmp/arenaguide.tmp'
 guidefile=$HOME/Escritorio/arenavision-guia.txt
-
-curl -s --cookie "beget=begetok"  http://arenavision.in/guide | html2text -width 100 > $guidetemp
+guideurl=$(curl -s --cookie "beget=begetok" http://arenavision.in | grep -o '\<a href.*\>' | sed 's/\<a\ href/\n\<a\ href/g' | grep EVENTS | cut -d '"' -f 2)
+curl -s --cookie "beget=begetok"  http://arenavision.in/$guideurl | html2text -width 100 > $guidetemp
 LNSTART=$(grep -n "EVENTS GUIDE" $guidetemp | cut -d ":" -f 1)
 LNEND=$(grep -n "Last update" $guidetemp | cut -d ":" -f 1)
 awk -v start="$LNSTART" -v end="$LNEND" 'NR >= start && NR <= end' $guidetemp > $guidefile
